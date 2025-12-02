8d7a33425af1715198b2a1b7d412965599ad0994
Arsalan Arish <mail@arsalanarish.com>
2025-12-02 18:42:56 +0500

    Misc.

--- Projects/ATLAS/atlas.py
+++ Projects/ATLAS/atlas.py
@@ -1,20 +1,69 @@
 from pathlib import Path
-
+from copy import deepcopy
 
 class atlas:
     
     def create_user(self, name):
-        lst = ['books.py','notes.py','tasks.py','user.py','scripts']
+
+        # Iniiating the necessary files and folders if not present
+        name = name.lower()
+        p1 = Path.home() / 'database'
+        p1.mkdir(parents=True, exist_ok=True)
+        temp = [n for n in p1.iterdir()]
+
+        if 'users.txt' in temp:
+            id = len(temp) # Debug this and the below line
+            print(id)
+            temp1 = p1 / 'users.txt'
+            user = f'[{id}, {name}]\n'
+            with open(temp1,"a") as f:
+                f.write(user)
+
+        if 'users.txt' not in temp:
+            temp1 = p1 / 'users.txt'
+            temp1.parent.mkdir(exist_ok=True,parents=True)
+            temp1.touch(exist_ok=True)
+            id = len(temp)+1
+            user = f'[{id}, {name}]\n'
+            with open(temp1,"a") as f:
+                f.write(user)
+
+
         
+        # Initiating user files
+
+        p2 = p1 / f'user_{name}_{id}'
+        p2.parent.mkdir(exist_ok=True, parents=True)
+        lst = ['books.py','notes.py','tasks.py','user_metadata.py','scripts']
+
         for i in lst:
-            p = Path('database') / f'user_{name}_' / f'{i}'
-            p.parent.mkdir(exist_ok=True, parents=True)
+            p3 = p2 / i
             if i.__contains__('.'):
-                p.touch()
+                p3.parent.mkdir(exist_ok=True,parents=True)
+                p3.touch(exist_ok=True)
             else:
-                p.mkdir()
+                p3.mkdir(exist_ok=True)
 
-        p = Path('database') / f'user_{name}' / 'user.py'
+        metadata = {
+            'id'   : id,
+            'name' : name,
+            'preferences' :    {},
+            'command_history': [],
+            'search_history':  []
+        }
+        
+        p4 = p2 / 'user_metadata.py'
+        with open(p4,"w") as p4:
+            p4.write(str(metadata))
 
-        with open(p,"w") as f:
-            pass
+        print(f"User registered successfully! See at {p2}")
+
+
+
+    def add_book(self, user_name, user_id, bookname=None, authorname=None):
+        pass
+        
+# Test
+x = atlas()
+x.create_user("Arsalan")
+
