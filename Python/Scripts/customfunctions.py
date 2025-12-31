2f7aa0b62b865d89029252f61b0ff3ef8a242488
Arsalan Arish <mail@arsalanarish.com>
2025-12-31 21:03:33 +0500

    Added qrcode & diamond printing functions

--- Python/Scripts/customfunctions.py
+++ Python/Scripts/customfunctions.py
@@ -80,3 +80,35 @@ def insertion_sort(Unsorted_List):
             i+=1 
 
     return Unsorted_List
+
+def qrcode_generator(url : str):
+    import qrcode
+
+    url = url.strip()
+    file_path = "qrcode.png"
+
+    qr = qrcode.QRCode()
+    qr.add_data(url)
+
+    img = qr.make_image()
+    img.save(file_path)
+    print("QR Code has been generated!")
+
+def diamond(n : int, fill : bool):
+    for i in range(2*n+1):
+      for j in range(2*n+1):
+        if i<=n:
+          if i+j==n or j-i==n:
+            print("x",end="")
+          elif j<n+i and j>n-i and fill==True:
+            print(":",end="")
+          else:
+            print(" ", end="")
+        else:
+          if i-j==n or i+j==3*n:
+            print("x", end="")
+          elif j<2*n-(i-n) and j>i-n and fill==True:
+            print(":",end="")
+          else:
+            print(" ",end="")
+      print()
